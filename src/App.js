169c25f06ae07db055fb8aff296426addd9b6f33
Antônio Santana <antonionetosg6@gmail.com>
2022-08-20 11:03:05 -0300

    Requisito #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components';
 
 class App extends React.Component {
   render() {
     // Iniciando
-    return (<p>Sistema Solar</p>);
+    return (<Header />);
   }
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>Sistema Solar</header>
+        )
+    }
+}
+
+/* class SolarSystem extends React.Component {
+    render(){
+        return(
+            <div data-testid="solar-system"></div>
+        )
+    }
+} */
+
+export default Header
