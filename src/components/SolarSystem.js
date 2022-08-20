b9dc8ade4e86b22624bce5ba7e2c0f4720070d34
Antônio Santana <antonionetosg6@gmail.com>
2022-08-20 11:23:01 -0300

    #3 e #4 [LINT]

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
-    render() {
-        return (
-            <div data-testid="solar-system">
-                <Title />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title />
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-function Title(headline){
+function Title(headline) {
   return (
     <h2>{headline}</h2>
-  )
+  );
 }
 
 export default Title;
